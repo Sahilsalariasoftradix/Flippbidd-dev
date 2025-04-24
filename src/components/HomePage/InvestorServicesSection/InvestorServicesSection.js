f877b650a632a8740dbf7352946c4b2410dc2dca
Sahilsalariasoftradix <Sahil.salaria@softradix.in>
2025-04-24 17:33:27 +0530

    Image changes

--- src/components/HomePage/InvestorServicesSection/InvestorServicesSection.js
+++ src/components/HomePage/InvestorServicesSection/InvestorServicesSection.js
@@ -12,51 +12,51 @@ const InvestorServicesSection = () => {
             <div >in Real-Time</div>
           </h2>
         </div>
-        
+
         <div className="services-cards">
           {/* FlippBidd Pro Services Card */}
           <div className="service-card pro-services-card" style={{ backgroundImage: `url(${IMAGES.FLIPPBIDD_PRO_BG})` }}>
             <div className="card-content text-start">
-            <div className='flex items-center gap-2 justify-between'>
-            <div className="icon-wrapper">
-                <img src={IMAGES.PERSON_ACHIEVEMENT} alt="Pro Services" className="service-icon" />
+              <div className='flex items-center gap-2 justify-between'>
+                <div className="icon-wrapper">
+                  <img src={IMAGES.PERSON_ACHIEVEMENT} alt="Pro Services" className="service-icon" />
+                </div>
+
+                <div className="service-email">
+                  <img src={IMAGES.GRADIENT_MAIL} alt="Email" className="email-icon" />
+                  <a href="mailto:LetsNetwork@flippbidd.com?subject=FlippBidd%20Pro-services" className='text-xs'>LetsNetwork@flippbidd.com</a>
+                </div>
               </div>
-              
-              <div className="service-email">
-                <img src={IMAGES.GRADIENT_MAIL} alt="Email" className="email-icon" />
-                <a href="mailto:LetsNetwork@flippbidd.com?FlippBidd%20Pro-services" className='text-xs'>LetsNetwork@flippbidd.com</a>
-              </div>
-            </div>
-              
+
               <h3 className="service-title text-[#000F1B]">FlippBidd Pro-Services</h3>
               <p className="service-description text-[#2B3842]">
                 A premium platform connecting real estate professionals with top-tier investment opportunities, tools, and resources.
               </p>
-              
-              <a href="mailto:LetsNetwork@flippbidd.com?FlippBidd%20Pro-services" className="contact-button white-button"><span>Contact Us</span></a>
+
+              <a href="mailto:LetsNetwork@flippbidd.com?subject=FlippBidd%20Pro-services" className="contact-button white-button"><span>Contact Us</span></a>
             </div>
           </div>
-          
+
           {/* National Lenders Card */}
           <div className="service-card before:!bg-none before:!content-[''] before:!bg-slate-50 before:opacity-[0.8] lenders-card" style={{ backgroundImage: `url(${IMAGES.NATIONAL_LENDERS_BG})` }}>
             <div className="card-content text-start">
-            <div className='flex items-center gap-2 justify-between'>
-            <div className="icon-wrapper">
-                <img src={IMAGES.NATIONAL_LENDER_ICON} alt="Pro Services" className="service-icon" />
+              <div className='flex items-center gap-2 justify-between'>
+                <div className="icon-wrapper">
+                  <img src={IMAGES.NATIONAL_LENDER_ICON} alt="Pro Services" className="service-icon" />
+                </div>
+
+                <div className="service-email">
+                  <img src={IMAGES.GRADIENT_MAIL} alt="Email" className="email-icon" />
+                  <a href="mailto:LetsNetwork@flippbidd.com?subject=National%20Lenders" className='text-xs'>LetsNetwork@flippbidd.com</a>
+                </div>
               </div>
-              
-              <div className="service-email">
-                <img src={IMAGES.GRADIENT_MAIL} alt="Email" className="email-icon" />
-                <a href="mailto:LetsNetwork@flippbidd.com?National%20Lenders" className='text-xs'>LetsNetwork@flippbidd.com</a>
-              </div>
-            </div>
-              
+
               <h3 className="service-title text-[#000F1B]">National Lenders</h3>
               <p className="service-description text-[#2B3842]">
                 Stay ahead of the Competition! Get Real-Time Leads of Investors Nationally right to your Inbox or CRM's when they view a real-estate investment.
               </p>
-              
-              <a href="mailto:LetsNetwork@flippbidd.com?National%20Lenders" className="contact-button gradient-button">Contact Us</a>
+
+              <a href="mailto:LetsNetwork@flippbidd.com?subject=National%20Lenders" className="contact-button gradient-button">Contact Us</a>
             </div>
           </div>
         </div>
